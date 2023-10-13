bcb05990f8885f33564f48a0ae149ff226c2f8b4
Danielle Barros Bassetto <danibbassetto@hotmail.com>
2023-10-13 10:09:10 -0300

    new

--- ZumbieLab/Data/DatabaseContext.cs
+++ ZumbieLab/Data/DatabaseContext.cs
@@ -6,9 +6,7 @@ namespace ZumbieLab.Data
 {
     public class DataBaseContext : DbContext
     {
-        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
-        {
-        }
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
 
         public DbSet<UserModel> User { get; set; }
 

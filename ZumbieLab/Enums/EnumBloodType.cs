82cb8a4a61685afb30ed34eb502a964a1e0b308f
Danielle Barros Bassetto <danibbassetto@hotmail.com>
2023-10-14 18:32:17 -0300

    new

--- ZumbieLab/Enums/EnumBloodType.cs
+++ ZumbieLab/Enums/EnumBloodType.cs
@@ -1,10 +1,25 @@
-﻿namespace ZombieLab.Enums
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ZombieLab.Enums
 {
     public enum EnumBloodType
     {
-        A,
-        B,
-        O,
-        AB
+        [Display(Name = "A+")]
+        APositive,
+        [Display(Name = "A-")]
+        ANegative,
+        [Display(Name = "B+")]
+        BPositive,
+        [Display(Name = "B-")]
+        BNegative,
+        [Display(Name = "O+")]
+        OPositive,
+        [Display(Name = "O-")]
+        ONegative,
+        [Display(Name = "AB+")]
+        ABPositive,
+        [Display(Name = "AB-")]
+        ABNegative
     }
 }

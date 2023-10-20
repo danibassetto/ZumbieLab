e51efc6cb990a34d001958b3dc77347b37dd4bd6
Danielle Barros Bassetto <danibbassetto@hotmail.com>
2023-10-19 23:56:54 -0300

    new

--- ZumbieLab/Models/Host/HostModel.cs
+++ ZumbieLab/Models/Host/HostModel.cs
@@ -16,11 +16,11 @@ namespace ZombieLab.Models
         public EnumGender? Gender { get; set; }
         [Required(ErrorMessage = "Informe o peso")]
         [Column(TypeName = "decimal(5,2)")]
-        [Range(1, int.MaxValue, ErrorMessage = "O peso deve ser maior que zero!")]
+        [Range(1, 999, ErrorMessage = "O peso deve ser maior que zero!")]
         public decimal? Weight { get; set; }
         [Required(ErrorMessage = "Informe a altura")]
-        [Column(TypeName = "decimal(5,2)")]
-        [Range(1, int.MaxValue, ErrorMessage = "A altura deve ser maior que zero!")]
+        [Column(TypeName = "decimal(4,2)")]
+        [Range(1, 99, ErrorMessage = "A altura deve ser maior que zero!")]
         public decimal? Height { get; set; }
         [Required(ErrorMessage = "Informe o tipo sanguíneo")]
         public EnumBloodType? BloodType { get; set; }
